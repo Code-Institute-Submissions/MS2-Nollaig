f83916575b5ffc0563a4e708f5e191d1395bf0b5
M-E-C-20 <mcasey8511@gmail.com>
2021-02-03 23:18:34 +0000

    Add zoomable map markers with location message

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,43 +1,38 @@
 function initMap() {
-    const myLatLng = { lat: 53.2734, lng: -7.77832031 };
-    const myLatLngBelfast = { lat: 54.596422950000004, lng: -5.929466360965295 };
-    const myLatLngGalway = { lat: 53.2744475, lng: -9.049333276783623 };
-    const myLatLngWicklow = { lat: 52.98072844827882, lng: -6.045849323272706 };
-    const myLatLngLimerick = { lat: 52.663611599999996, lng: -8.622099200094345 };
-    const myLatLngWaterford = { lat: 52.26023187747952, lng: -7.10592806339264 };
-    const myLatLngCork = { lat: 51.896661748557364, lng: -8.474707603454592 };
-    const map = new google.maps.Map(document.getElementById("map"), {
+    var locations = [
+        ['Belfast Continental Christmas Market', 54.596422950000004, -5.929466360965295, 4],
+        ['Christmas Market Galway', 53.2744475, -9.049333276783623, 5],
+        ['Wicklow Christmas Market', 52.98072844827882, -6.045849323272706, 3],
+        ['Milk Market Limerick', 52.663611599999996, -8.622099200094345, 2],
+        ['Cork Glow', 51.896661748557364, -8.474707603454592, 1],
+        ['Waterford Winterval', 52.26023187747952, -7.10592806339264, 6]
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 6,
-        center: myLatLng,
+        center: new google.maps.LatLng(53.2734, -7.77832031),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-    new google.maps.Marker({
-        position: myLatLngBelfast,
-        map,
-        title: "Belfast",
-    });
-    new google.maps.Marker({
-        position: myLatLngGalway,
-        map,
-        title: "Galway",
-    });
-    new google.maps.Marker({
-        position: myLatLngWicklow,
-        map,
-        title: "Wicklow",
-    });
-    new google.maps.Marker({
-        position: myLatLngLimerick,
-        map,
-        title: "Limerick",
-    });
-    new google.maps.Marker({
-        position: myLatLngWaterford,
-        map,
-        title: "Waterford",
-    });
-    new google.maps.Marker({
-        position: myLatLngCork,
-        map,
-        title: "Cork",
-    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function (marker, i, locations) {
+            return function () {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+                map.setZoom(10);
+                map.setCenter(marker.getPosition(locations[1][2]));
+            }
+        })(marker, i, locations));
+    }
 }
+
+
